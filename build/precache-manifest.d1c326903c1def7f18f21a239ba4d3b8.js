e418856f1931b0a15351cb45e68e529d428ba81b
杨麒平 <45160548+yangqiping@users.noreply.github.com>
2020-11-09 16:19:23 +0800

    Add files via upload

---
+++ build/precache-manifest.d1c326903c1def7f18f21a239ba4d3b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e27db2c19076500c5a0dff68c253c20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "984ca7f85de4f29ec820",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "dae2a28f397d938842c7",
+    "url": "/static/css/main.7f8e0395.chunk.css"
+  },
+  {
+    "revision": "984ca7f85de4f29ec820",
+    "url": "/static/js/2.0a330192.chunk.js"
+  },
+  {
+    "revision": "de5fa9cfc3624bea3d7565f048bfaaaf",
+    "url": "/static/js/2.0a330192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae2a28f397d938842c7",
+    "url": "/static/js/main.b42f18f6.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "6b55396ec634fbe26567a34c9751831f",
+    "url": "/static/media/taobao.6b55396e.PNG"
+  }
+]);
